1fa036bbcbbc2220fa5b63e1bf48ae92239769a3
Thomas Quetier <mamangvomi@gmail.com>
2021-04-22 18:36:39 +0200

    Init projet

---
+++ src/main/java/com/example/ctwebserv/CtwebservApplication.java
@@ -0,0 +1,13 @@
+package com.example.ctwebserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtwebservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtwebservApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ctwebserv/CtwebservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ctwebserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtwebservApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
